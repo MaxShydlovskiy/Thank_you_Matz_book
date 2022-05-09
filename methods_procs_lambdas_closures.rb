367e73d117f3f6fae8e915d6a316785a02ab8dc5
MaxShydlovskiy <shidlymax@gmail.com>
2022-05-09 14:45:34 +0300

    five attempt

--- methods_procs_lambdas_closures.rb
+++ methods_procs_lambdas_closures.rb
@@ -385,11 +385,201 @@ puts doubler.call([1,2,3])   # Now this prints 3,6,9!
 # So we could replace the eval invocation above wich:
 eval("n=3", doubler)
 
+# Compute the average and standard deviation of an array of numbers
+mean = a.inject {|x,y| x+y } / a.size
+sumOfSquares = a.map{|x| (x-mean)**2 }.inject{|x,y| x+y }
+standardDeviation = Math.sqrt(sumOfSquares/(a.size-1))
+
+# This module defines methods and operators for functional programming.
+module Functional
+  # Apply this function to each element of the specified Enumerable,
+  # returning an array of results. This is the reverse of Enumerable.map.
+
+  # Use | as an operator alias. Read "|" as "over" or "applied over".
+#
+# Example:
+# a = [[1,2],[3,4]]
+# sum = lambda {|x,y| x+y}
+# sums = sum|a
+# => [3,7]
+  def apply(enum)
+    enum.map &self
+  end
+  alias | apply
+# Use this function to "reduce" an enumerable to a single quantity.
+# This is the inverse of Enumerable.inject.
+# Use <= as an operator alias.
+# Mnemonic: <= looks like a needle for injections
+# Example:
+# data = [1,2,3,4]
+# sum = lambda {|x,y| x+y}
+# total = sum<=data
+# => 10
+  def reduce(enum)
+    enum.inject &self
+  end
+  alias <= reduce
+end
 
 
+sum = lambda {|x,y| x+y }                     # A function to add two numbers
+mean = (sum<=a)/a.size                        # Or sum.reduce(a) or a.inject(&sum)
+deviation = lambda {|x| x-mean }              # Function to compute difference from mean
+square = lambda {|x| x*x }                    # Function to square a number
+standardDeviation = Math.sqrt((sum<=square|(deviation|a))/(a.size-1))
 
 
+module Functional
+  # Return a new lambda that computes self[f[args]].
+  # Use * as an operator alias for compose.
+  # Examples, using the * alias for this method.
+  #
+  # f = lambda {|x| x*x }
+  # g = lambda {|x| x+1 }
+  # (f*g)[2]
+  # => 9
+  # (g*f)[2]
+  # => 5
+  #
+  # def polar(x,y)
+  # [Math.hypot(y,x), Math.atan2(y,x)]
+  # end
+  # def cartesian(magnitude, angle)
+  # [magnitude*Math.cos(angle), magnitude*Math.sin(angle)]
+  # end
+  # p,c = method :polar, method :cartesian
+  # (c*p)[3,4] # => [3,4]
+  #
+  def compose(f)
+    if self.respond_to?(:arity) && self.arity == 1
+      lambda {|*args| self[f[*args]] }
+    else
+      lambda {|*args| self[*f[*args]] }
+    end
+  end
+  # * is the natural operator for function composition.
+  alias * compose
+end
 
+# Using the same definitions of the lambdas sum , square , and deviation , the computation becomes:
+standardDeviation = Math.sqrt((sum<=square*deviation|a)/(a.size-1))
+
+product = lambda {|x, y| x*y }        # A function of two arguments
+double = lambda {|x| product(2,x) }   # Apply one argument
+
+module Functional
+  #
+  # Return a lambda equivalent to this one with one or more initial
+  # arguments applied. When only a single argument
+  # is being specified, the >> alias may be simpler to use.
+  # Example:
+  #
+  product = lambda {|x,y| x*y}
+  #
+  doubler = lambda >> 2
+  #
+  def apply_head(*first)
+    lambda {|*rest| self[*first.concat(rest)]}
+  end
+  #
+  # Return a lambda equivalent to this one with one or more final arguments
+  # applied. When only a single argument is being specified,
+  # the << alias may be simpler.
+  # Example:
+  # difference = lambda {|x,y| x-y }
+  # decrement = difference << 1
+  #
+  def apply_tail(*last)
+    lambda {|*rest| self[*rest.concat(last)]}
+  end
+  # Here are operator alternatives for these methods. The angle brackets
+  # point to the side on which the argument is shifted in.
+  alias >> apply_head     # g = f >> 2 -- set first arg to 2
+  alias << apply_tail     # g = f << 2 -- set last arg to 2
+end
+
+
+difference = lambda {|x,y| x-y }  # Compute difference of two numbers
+deviation = difference<<mean      # Apply second argument
+
+
+module Functional
+  #
+  # Return a new lambda that caches the results of this function and
+  # only calls the function when new arguments are supplied.
+  #
+  def memoize
+    cache = {} # An empty cache. The lambda captures this in its closure.
+    lambda {|*args|
+      # notice that the hash key is the entire array of arguments!
+      unless cache.has_key?(args) # If no cached result for these args
+        cache[args] = self[*args] # Compute and cache the result
+      end
+      cache[args]
+      # Return result from cache
+    }
+  end
+  # A (probably unnecessary) unary + operator for memoization
+  # Mnemonic: the + operator means "improved"
+  alias +@ memoize        # cached_f = +f
+end
+
+
+# A memoized recursive factorial function
+factorial = lambda {|x| return 1 if x==0; x*factorial[x-1]; }.memoize
+# Or, using the unary operator syntax
+factorial = +lambda {|x| return 1 if x==0; x*factorial[x-1]; }
+
+factorial = lambda {|x| return 1 if x==0; x*factorial[x-1]; }
+cached_factorial = +factorial # Recursive calls aren't cached!
+
+# Increment an array of integers with the Fixnum.succ method
+[1,2,3].map(&:succ) # => [2,3,4]
+
+[1,2,3].map {|n| n.succ }
+
+class Symbol
+  def to_proc
+    lambda {|receiver, *args| receiver.send(self, *args)}
+  end
+end
+
+class Symbol
+  def to_proc
+    lambda {|receiver, *args| receiver.method(self)[*args]}
+  end
+end
+
+class Module
+  # Access instance methods with array notation. Returns UnboundMethod,
+  alias [] instance_method
+end
+
+class UnboundMethod
+  # Allow [] as an alternative to bind.
+  alias [] bind
+end
+
+String[:reverse]["hello"][]   # => "olleh"
+
+class Module
+  # Define a instance method with name sym and body f.
+  # Example: String[:backwards] = lambda { reverse }
+  def []=(sym, f)
+    self.instance_eval { define_method(sym, f) }
+  end
+end
+
+
+Enumerable[:average] = lambda do
+  sum, n = 0.0, 0
+  self.each {|x| sum += x; n += 1 }
+  if n == 0
+    nil
+  else
+    sum/n
+  end
+end
 
 
 
